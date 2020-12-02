ad3688a04d29a30a93e9e7ed56e3166163997329
Chase Snider <jcsnider@outlook.com>
2020-12-01 22:14:14 -0500

    finshed

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Your code goes here
 const nav = document.querySelectorAll(".nav");
-const logo = document.querySelector(".logo-heading");
+const logo = document.querySelector("h1");
 const intro = document.querySelector(".intro");
-const content = document.querySelector(".text-content p");
-const imgContent = document.querySelectorAll(".img-content");
+const content = document.querySelectorAll("p");
+const imgContent = document.querySelectorAll(".img");
 const text = document.querySelectorAll("p");
 
 
@@ -22,38 +22,87 @@ nav[0].addEventListener("click", event =>{
     },500);
 },false);
 
-logo.addEventListener("keydown", event =>{
-    if(event.key === "Escape"){
+logo.addEventListener("contextmenu", event =>{
+    event.target.style.textTransform = "capitalize";
      
     }
-});
-intro.addEventListener('dblclick', event =>{
-    event.target.style.textShadow = "5px 5px 1px yellow, 10px 10px 1px #0000ff";
-});
+);
+// intro.addEventListener('dblclick', event =>{
+//     event.target.style.textShadow = "5px 5px 1px yellow, 10px 10px 1px #0000ff";
+// });
+Array.from(content).forEach((int)=>{
+    int.addEventListener('dblclick', event =>{
+        event.target.style.textShadow = "5px 5px 1px yellow, 10px 10px 1px #0000ff"; 
+        setTimeout(()=>{
+            event.target.style.textShadow = "";
+        },600)
+    },false)
+})
 
-imgContent[0].addEventListener("mouseout", event =>{
-    event.target.style.transform = 'rotate(7deg)';
-    setTimeout(function(){
-        event.target.style.transform = 'rotate(0deg)';
-    },500)
+Array.from(imgContent).forEach((img)=>{
+    img.addEventListener('mouseout', event =>{
+        event.target.style.transform = "rotate(7deg)";
+        setTimeout(()=>{
+            event.target.style.transform = "rotate(-7deg)"
+        },250)
+
+    },false)
+})
+// this before i figured out .forEach
+// imgContent[0].addEventListener("mouseout", event =>{
+//     event.target.style.transform = 'rotate(7deg)';
+//     setTimeout(function(){
+//         event.target.style.transform = 'rotate(0deg)';
+//     },500)
     
-},false);
+// },false);
 
-imgContent[1].addEventListener("mouseout", event =>{
-    event.target.style.transform = 'rotate(7deg)';
-    setTimeout(function(){
-        event.target.style.transform = 'rotate(0deg)';
-    },500)
+// imgContent[1].addEventListener("mouseout", event =>{
+//     event.target.style.transform = 'rotate(7deg)';
+//     setTimeout(function(){
+//         event.target.style.transform = 'rotate(0deg)';
+//     },500)
     
-},false);
+// },false);
 
-document.addEventListener("mouseleave", event =>{
+document.addEventListener("mousemove", event =>{
     event.target.style.backgroundColor = "yellow";
     setTimeout(function(){
         event.target.style.backgroundColor = "white"
     },100)
 },false);
 
-// document.addEventListener("mouseleave", event =>{
-//     event.target.st
+// content.addEventListener("mouseenter", event =>{
+//     event.target.style.fontSize = "x-large";
 // })
+
+Array.from(content).forEach((link) => {
+    link.addEventListener('mouseover', event => {
+        event.target.style.fontSize = "x-large";
+        setTimeout(()=>{
+            event.target.style.fontSize = "";
+        },1000)
+        // event.preventDefault();
+    },false)
+});
+
+window.addEventListener("scroll", (event) =>{
+    let scroll = window.scrollY;
+    if(scroll > 300){
+        document.querySelectorAll("h1").forEach((para) =>{
+            para.style.outline = "thick solid yellow"; 
+        });
+        
+    }else{
+        document.querySelectorAll("h1").forEach((para)=>{
+            para.style.outline= "thick solid blue"; 
+    })}
+     
+    // setInterval(() => {
+    //     let 
+    // }, interval);
+});
+
+window.addEventListener("load", (event)=>{
+    
+})
